c8df48800d36c1a04a0cee3ac0ab24eecfb87766
Joaquin Galoppo <joaquingaloppo@gmail.com>
2023-05-15 16:56:41 -0300

    Add card services

---
+++ server/src/services/card_service.js
@@ -0,0 +1,18 @@
+const Card = require("../models/card");
+
+exports.get_cards = (user) => Card.find({ user });
+
+exports.create_card = (name, description, type, number, expiration_date, cvv, user) =>
+  Card.create({
+    name,
+    description,
+    type,
+    number,
+    expiration_date,
+    cvv,
+    user,
+  });
+
+exports.delete_card = (id) => Card.findByIdAndDelete(id);
+
+exports.update_card = (id, data) => Card.findByIdAndUpdate(id, data);
