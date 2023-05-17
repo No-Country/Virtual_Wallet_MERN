f4e9d9414908c52b91d8d8e999ce109962174f62
cristian1534 <cmachuca32@gmail.com>
2023-05-17 12:02:14 +0000

    Ansible->DockerHub, Deploy/Service K8s

--- server/src/server.js
+++ server/src/server.js
@@ -21,10 +21,17 @@ app.use(body_parser.json());
 app.use(cors(cors_options));
 app.use(morgan("dev"));
 
+app.get("/", (req, res) => {
+  try {
+    res.send("PENGUIN-WALLET SERVER RUNNING.");
+  } catch (err) {
+    return res.sendStatus(500);
+  }
+});
 app.use("/api/", auth_routes);
 app.use("/api/user", user_routes);
 app.use("/api/card", card_routes);
-app.use("/api/transaction", transaction_routes)
+app.use("/api/transaction", transaction_routes);
 
 const PORT = process.env.PORT || 5000;
 
