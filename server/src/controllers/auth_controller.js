af8245b6e54f95fe00a1f456c89e034f01e2cc48
Gabriel <gszcode@gmail.com>
2023-05-18 17:21:49 -0300

    eliminacion de console.log

--- server/src/controllers/auth_controller.js
+++ server/src/controllers/auth_controller.js
@@ -1,43 +1,45 @@
-const { create_user, get_user_by_email } = require("../services/auth_service");
-const { create_token } = require("../helpers/token_creator");
-const bcrypt = require("bcrypt");
+const { create_user, get_user_by_email } = require('../services/auth_service')
+const { create_token } = require('../helpers/token_creator')
+const bcrypt = require('bcrypt')
 
 exports.register = async (req, res) => {
   try {
-    const { name, surname, username, email } = req.body;
-    if ((!name, !surname, !username, !email)) return res.sendStatus(400);
+    const { name, surname, username, email } = req.body
+    if ((!name, !surname, !username, !email)) return res.sendStatus(400)
 
-    const registered_user = await get_user_by_email(email);
-    if (registered_user) return res.status(400).json({ message: "EMAIL YA REGISTRADO"});
+    const registered_user = await get_user_by_email(email)
+    if (registered_user)
+      return res.status(400).json({ message: 'EMAIL YA REGISTRADO' })
 
-    const salt = 10;
+    const salt = 10
     const password = await bcrypt.hash(req.body.password, salt)
 
-    const user = await create_user({ name, surname, username, email, password });
-    return res.status(200).json(user).end();
+    const user = await create_user({ name, surname, username, email, password })
+    return res.status(200).json(user).end()
   } catch (err) {
-    return res.sendStatus(500);
+    return res.sendStatus(500)
   }
-};
-
+}
 
 exports.login = async (req, res) => {
-  try{
-    const { email, password } = req.body;
+  try {
+    const { email, password } = req.body
 
-    const user = await get_user_by_email(email);
-    if(!user) return res.status(404).json({ message: "USUARIO NO ENCONTRADO"});
+    const user = await get_user_by_email(email)
+    if (!user) return res.status(404).json({ message: 'USUARIO NO ENCONTRADO' })
 
-    const validated_password = await bcrypt.compare( password, user.password );
-    if(!validated_password) return res.status(400).json({ message: "USUARIO O CONTRASEÑA INCORRECTA" })
+    const validated_password = await bcrypt.compare(password, user.password)
+    if (!validated_password)
+      return res
+        .status(400)
+        .json({ message: 'USUARIO O CONTRASEÑA INCORRECTA' })
 
-    const token = create_token(user);
+    const token = create_token(user)
     const { _id, username } = user
 
-    return res.status(200).json({ _id, username, token });
-
-  }catch(err){
+    return res.status(200).json({ _id, username, token })
+  } catch (err) {
     console.log(err.message)
-    return res.sendStatus(500);
+    return res.sendStatus(500)
   }
 }
