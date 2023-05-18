7cba3fd8f8f040a916d701c496a65e10a45dacc4
guille <guillermoneculqueo@gmail.com>
2023-05-18 12:08:58 -0300

    landing 1.0 banner, logo, title, cards..

---
+++ client/src/components/ItemCard.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+export const ItemCard = ({ element }) => {
+  return (
+    <>
+      <Link to={`/${element.name}`} className='itemCard w-[300px] h-[100px] sm:w-[150px] sm:h-[150px] border-1 border-c-subtitulo rounded-[4px] p-2 bg-c-titulo flex sm:flex-col items-center justify-around' style={{ boxShadow: '10px 10px 9px #cccccc, -10px -10px 9px #f4f4f4' }} >
+        <h2 className='w-1/2 sm:w-full font-titulo text-sm text-c-contenedor'>{element.service}</h2>
+        <div className='w-1/2 sm:w-full flex items-center justify-end h-auto'><span className='text-c-boton-accion' style={{fontSize:'1rem'}} dangerouslySetInnerHTML={{ __html: element.svg}} ></span></div>
+      </Link>
+    </>
+  )
+}
+
