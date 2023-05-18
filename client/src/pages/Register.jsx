7f54c90c52bd6f1c5af4e4d04994d09341405db1
Gabriel <gszcode@gmail.com>
2023-05-18 16:00:23 -0300

    logo agregado

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,17 @@
 import { Link } from 'react-router-dom'
+const logo =
+  'https://res.cloudinary.com/dpiwmbsog/image/upload/v1684361186/wallet/A_vibrant_and_energetic_scene_of_a_reggaeton_pengu_h2o30b.jpg'
 
 const Register = () => {
   return (
     <section className="w-full h-screen flex flex-col justify-start items-center gap-15 mt-24">
-      <span>LOGO</span>
-      <h2>
-        Registrarse en <span>Pingui-Wallet</span>
+      <img
+        className="rounded-[50%] object-cover w-[95px] h-[90px]"
+        src={logo}
+        alt="Logo PinguiWallet"
+      />
+      <h2 className="text-[#FC7E09] text-2xl font-bold">
+        Registrarse en <span>PinguiWallet</span>
       </h2>
       <form className="flex flex-col gap-8 py-14 px-20">
         <div className="w-80 h-10 border-b border-shadow">
