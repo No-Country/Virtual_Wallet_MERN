0839622c26a1b4cef324f5fd6c188fb3ac733685
Gabriel <gszcode@gmail.com>
2023-05-18 16:02:29 -0300

    texto modificado

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,22 +1,21 @@
-import { Banner } from "./Banner"
-import { SeparatorLogo } from "./SeparatorLogo"
-import { GenericTitle } from "./GenericTitle"
-import { ItemCardList } from "./ItemCardList.jsx"
-import { Faqs } from "./Faqs"
+import { Banner } from './Banner'
+import { SeparatorLogo } from './SeparatorLogo'
+import { GenericTitle } from './GenericTitle'
+import { ItemCardList } from './ItemCardList.jsx'
+import { Faqs } from './Faqs'
 
 export const Main = ({ children }) => {
   return (
     <>
-      <main className= "my-2 bg-c-fondo w-full h-auto flex flex-col gap-10" >
+      <main className="my-2 bg-c-fondo w-full h-auto flex flex-col gap-10">
         <Banner />
         <SeparatorLogo />
-        <GenericTitle title="Te simplificamos el trabajo!!!"/>
+        <GenericTitle title="Te simplificamos el trabajo!" />
         <ItemCardList />
-        <GenericTitle title="¿Que mas debo saber?"/>
+        <GenericTitle title="¿Qué más debo saber?" />
         <Faqs />
         {children}
       </main>
     </>
   )
 }
-
