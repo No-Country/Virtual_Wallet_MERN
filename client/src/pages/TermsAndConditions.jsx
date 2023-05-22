056bd4659190bfc8ac2d123c217d0dd3e7425857
Gabriel <gszcode@gmail.com>
2023-05-22 16:49:58 -0300

    componente de terminos y condiciones creado

---
+++ client/src/pages/TermsAndConditions.jsx
@@ -0,0 +1,138 @@
+const TermsAndConditions = () => {
+  return (
+    <section className="w-full flex flex-col justify-center items-center text-center my-20">
+      <h1 className="font-bold text-3xl bg-white">Términos y Condiciones</h1>
+      <div className="my-10 w-[70%] text-start bg-twitter p-5">
+        <p>
+          Bienvenido a{' '}
+          <span className="text-c-titulo font-semibold">PingüiWallet</span>.
+          Estos términos y condiciones regulan tu uso de nuestro sitio web. Al
+          acceder y utilizar nuestro sitio web, aceptas cumplir con estos
+          términos y condiciones. Si no estás de acuerdo con alguno de los
+          términos establecidos aquí, te recomendamos que no utilices nuestro
+          sitio web.
+        </p>
+        <h4 className="font-semibold mt-10 mb-5">1. Uso del Sitio Web</h4>{' '}
+        <p>
+          <ul>
+            <li>
+              a. El uso de nuestro sitio web se rige por las leyes y
+              regulaciones aplicables. No debes utilizar nuestro sitio web con
+              fines ilegales o no autorizados.
+            </li>{' '}
+            <br />
+            <li>
+              b. Al utilizar nuestro sitio web, te comprometes a no infringir
+              los derechos de propiedad intelectual de terceros ni a realizar
+              actividades que puedan dañar, interferir o comprometer la
+              seguridad de nuestro sitio web.
+            </li>{' '}
+            <br />
+            <li>
+              c. Nos reservamos el derecho de modificar, suspender o
+              descontinuar nuestro sitio web en cualquier momento y sin previo
+              aviso.
+            </li>{' '}
+            <br />
+          </ul>
+        </p>
+        <h4 className="font-semibold mt-10 mb-5">2. Contenido del Sitio Web</h4>{' '}
+        <p>
+          <ul>
+            <li>
+              a. El contenido de nuestro sitio web es proporcionado con fines
+              informativos y educativos. No garantizamos la exactitud,
+              integridad o actualidad del contenido.
+            </li>{' '}
+            <br />
+            <li>
+              b. No nos hacemos responsables de cualquier daño o perjuicio
+              derivado del uso del contenido de nuestro sitio web.
+            </li>{' '}
+            <br />
+            <li>
+              c. Nos reservamos el derecho de modificar, actualizar o eliminar
+              cualquier contenido de nuestro sitio web sin previo aviso.
+            </li>{' '}
+            <br />
+          </ul>
+        </p>
+        <h4 className="font-semibold mt-10 mb-5">
+          3. Enlaces a Sitios Externos
+        </h4>{' '}
+        <p>
+          <ul>
+            <li>
+              a. Nuestro sitio web puede contener enlaces a sitios web de
+              terceros. Estos enlaces son proporcionados únicamente como
+              referencia y no implican nuestro respaldo o afiliación con esos
+              sitios web.
+            </li>{' '}
+            <br />
+            <li>
+              b. No somos responsables del contenido, las políticas de
+              privacidad o las prácticas de los sitios web de terceros. Te
+              recomendamos leer las políticas y términos de uso de esos sitios
+              antes de interactuar con ellos.
+            </li>{' '}
+            <br />
+          </ul>
+        </p>
+        <h4 className="font-semibold mt-10 mb-5">4. Privacidad</h4>{' '}
+        <p>
+          <ul>
+            <li>
+              a. Tu privacidad es importante para nosotros. Nuestra política de
+              privacidad describe cómo recopilamos, utilizamos y protegemos tu
+              información personal al utilizar nuestro sitio web. Al utilizar
+              nuestro sitio web, aceptas nuestras prácticas de privacidad según
+              lo establecido en nuestra política de privacidad.
+            </li>{' '}
+          </ul>
+        </p>
+        <h4 className="font-semibold mt-10 mb-5">
+          5. Limitación de Responsabilidad
+        </h4>{' '}
+        <p>
+          <ul>
+            <li>
+              a. En la medida máxima permitida por la ley, no seremos
+              responsables de ningún daño directo, indirecto, incidental,
+              especial o consecuente que surja del uso de nuestro sitio web. b.
+              No garantizamos que nuestro sitio web estará libre de errores,
+              virus u otros componentes dañinos.
+            </li>{' '}
+          </ul>
+        </p>
+        <h4 className="font-semibold mt-10 mb-5">6. Propiedad Intelectual</h4>{' '}
+        <p>
+          <ul>
+            <li>
+              a. Todo el contenido presente en nuestro sitio web, incluyendo
+              pero no limitado a texto, gráficos, logotipos, imágenes, videos y
+              software, está protegido por derechos de propiedad intelectual y
+              leyes de derechos de autor.
+            </li>{' '}
+            <li>
+              <br />
+              b. No puedes utilizar, reproducir, modificar o distribuir el
+              contenido de nuestro sitio web sin nuestra autorización previa y
+              por escrito.
+            </li>{' '}
+          </ul>
+        </p>
+        <br />
+        <br />
+        <p>
+          Estos términos y condiciones constituyen el acuerdo completo entre tú
+          y <span className="text-c-titulo font-semibold">PingüiWallet</span>
+          con respecto a tu uso de nuestro sitio web. Si tienes alguna pregunta
+          o inquietud acerca de estos términos y condiciones, no dudes en
+          contactarnos.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default TermsAndConditions
