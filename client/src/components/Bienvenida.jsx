2c72e84e65e264c2737b184a14dc84817a420fe4
guille <guillermoneculqueo@gmail.com>
2023-05-24 20:00:09 -0300

    no se habian guardado bienvenida

--- client/src/components/Bienvenida.jsx
+++ client/src/components/Bienvenida.jsx
@@ -1,21 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 const Bienvenida = () => {
   const user = useSelector((state) => state.user.user);
-  const [statea, setStatea] = useState(false)
 
   useEffect(() => {
     console.log("Bienvenida ",user)
-    setStatea(true);
-    console.log("Bienvenida ",user)
-  
-  },[])
+
+  },[user])
 
   console.log("Bienvenida ",user)
   return (
     <section className="saludo flex flex-row items-center justify-center h-auto w-full">
-    {statea ? <h1>Bienvenido {user.username} {user.surname}</h1> : <h1>Bienvenido</h1>}
+    {user ? <h1>Bienvenido {user.username} {user.surname}</h1> : <h1>Bienvenido</h1>}
     </section>
   )
 }
