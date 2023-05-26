64d010883d87940d00bb7d91775ad669bdc097fd
guille <guillermoneculqueo@gmail.com>
2023-05-26 20:59:05 -0300

    seccion usuario, update user

--- client/src/slices/userSlice.js
+++ client/src/slices/userSlice.js
@@ -16,15 +16,63 @@ export const fetchUserByid = createAsyncThunk(
   }
 );
 
+//accion asincronica para actualizar un usuario
+export const updateUser = createAsyncThunk(
+  'user/updateUser',
+  async ({ userId, userData }) => {
+    const response = await fetch(`${API_URL}/update-one/${userId}`, {
+      method : 'PATCH',
+      headers : {
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify(userData),
+    });
+    if(!response.ok){
+      throw new Error('Error al actualizar el usuario');
+    }
+    const data = await response.json();
+    return data;
+  }
+);
+
+// Acción asincrónica para eliminar un usuario
+export const deleteUser = createAsyncThunk(
+  'user/deleteUser',
+  async (userId) => {
+    const response = await fetch(`${API_URL}/delete-one/${userId}`, {
+      method: 'DELETE',
+    });
+    if (!response.ok) {
+      throw new Error('Error al eliminar el usuario');
+    }
+    const data = await response.json();
+    return data;
+  }
+);
+
 const userSlice = createSlice({
   name: 'user',
   initialState: {
     user: null,
     loading: false,
     error: null,
+    deleted: false, 
+    updated: false,
   },
   reducers: {
-    // ... Otras acciones y reducers ...
+     // Acción para limpiar el estado del usuario
+    clearUser(state) {
+      state.user = null;
+      state.loading = false;
+      state.error = null;
+      state.deleted = false;
+      state.updated = false;
+    },
+
+    // Acción para marcar el usuario como eliminado
+    markUserAsDeleted(state) {
+      state.deleted = true;
+    },
 
   },
   extraReducers: (builder) => {
@@ -40,11 +88,41 @@ const userSlice = createSlice({
       .addCase(fetchUserByid.rejected, (state, action) => {
         state.loading = false;
         state.error = action.error.message;
+      })
+      .addCase(updateUser.pending, (state) => {
+        state.loading = true;
+        state.updated = false;
+        state.error = null;
+      })
+      .addCase(updateUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.updated = true;
+        // Actualizar los datos del usuario si es necesario
+        state.user = action.payload;
+      })
+      .addCase(updateUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+      .addCase(deleteUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(deleteUser.fulfilled, (state) => {
+        state.loading = false;
+        state.deleted = true;
+        // Realizar acciones adicionales si es necesario
+      })
+      .addCase(deleteUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
       });
   }
 });
 
 // ... Exportar acciones y reducer ...
 export const selectUser = (state) => state.user.user;
+
+export const { clearUser, markUserAsDeleted } = userSlice.actions;
 
 export default userSlice.reducer;
