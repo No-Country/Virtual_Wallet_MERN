082fb4fbcbd92133fe0ca02eec05e86437ede4a3
guille <guillermoneculqueo@gmail.com>
2023-05-25 23:56:39 -0300

    cambios faltantes

--- client/src/components/CardsAdd.jsx
+++ client/src/components/CardsAdd.jsx
@@ -69,7 +69,7 @@ const CardsAdd = () => {
             {cardDePrueba.numero}
           </div>
         </div>
-        <Link to={'/creditCard'}
+        <Link to={'/home/creditCard'}
           className="mt-4 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
           // onClick={agregarTarjeta}
         >
