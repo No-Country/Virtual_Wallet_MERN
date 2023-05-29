af58f8af0393cb1f9b98a46ff128a4079174f6bf
guille <guillermoneculqueo@gmail.com>
2023-05-29 03:22:52 -0300

    crud tarjeta andando, falta poder modificar

--- client/src/components/CreditCard.jsx
+++ client/src/components/CreditCard.jsx
@@ -1,16 +1,57 @@
 import { useState } from "react";
+import CardSaldo from "./CardSaldo";
+import { useDispatch } from "react-redux";
+import { deleteCard } from "../slices/cardSlice";
 
 const CreditCard = ({ objeto }) => {
   const [isFlipped, setIsFlipped] = useState(false);
+  const dispatch = useDispatch();
+  const [successMessage, setSuccessMessage] = useState('');
+  const [isConfirmed, setIsConfirmed] = useState(false);
+
+  const handleDelete = () => {
+
+    dispatch(deleteCard(objeto._id))
+    .then((res) => {
+      console.log("respuesta ->", res);
+      setSuccessMessage('Eliminado con éxito');
+    })
+    .catch((error) => {
+      console.log("error ->", error);
+      setSuccessMessage('Error al eliminar');
+    })
+    .finally(() => {
+      console.log(successMessage)
+      setIsConfirmed(false);
+      setTimeout(() => {
+        setSuccessMessage('');
+      }, 4000);
+    })
+  }
   
   const flipCard = () => {
     setIsFlipped(!isFlipped);
   }
-  const backgroundImage = 'https://res.cloudinary.com/dpiwmbsog/image/upload/v1684718648/wallet/pinguiWallet-bg-prompt-hero1_cke13p.jpg'
 
   return (
-    <div className="bg-c-fuente-secundario w-full h-[400px] p-6 flex justify-center items-center gap-6 bg-[#153367;]" >
-      <div className={`flip-card bg-transparent  w-[350px] h-[200px] rounded-lg border-solid overflow-hidden perspective-1000 relative `} onClick={flipCard}>
+    <div className="bg-[#333] w-full max-w-[450px] sm:w-[420px] h-auto p-5 flex flex-col justify-start items-center gap-5 rounded-[4px] relative" >
+      {isConfirmed && (
+          <div className="modal w-full h-auto p-2 text-center">
+            <div className="modal-content">
+              <h3 className="text-colorFuente2Submenu font-parrafo">⚠️ ¿esta seguro de eliminar la tarjeta?</h3>
+              <div className="flex flex-row justify-center items-center gap-2">
+                <button onClick={handleDelete} className="font-parrafo text-colorFuente2Submenu bg-colorFuente1Submenu w-[60px] rounded-[4px] p-1 hover:bg-hoverBotonSubmenu hover:text-colorFuente1Submenu transition duration-300 ease-in-out">Si</button>
+                <button onClick={()=> setIsConfirmed(false)} className="font-parrafo text-colorFuente2Submenu bg-colorFuente1Submenu w-[60px] rounded-[4px] p-1 hover:bg-hoverBotonSubmenu hover:text-colorFuente1Submenu transition duration-500 ease-in-out">No</button>
+              </div>
+            </div>
+          </div>
+        )}
+        {successMessage && (
+        <div className="modal w-full h-auto p-2 text-center">
+          <h3 className="text-colorFuente2Submenu font-parrafo text-green-400">🤯 Tarjeta eliminada con exito!!!</h3>
+        </div>
+        )}
+      <div className={`flip-card bg-transparent w-[300px] h-[200px] sm:w-[320px] sm:h-[200px] rounded-lg border-solid overflow-hidden perspective-1000 relative `} onClick={flipCard}>
           <section className={`flip-card-inner w-[100%] h-[100%] transition-transform duration-500 ease-in-out relative`} style={{transformStyle: 'preserve-3d', transform: isFlipped ? 'rotateY(180deg)' : 'rotateY(0deg)'}}>
             
             <section className="flip-card-fron w-[100%] h-[100%] absolute top-0 left-0 text-white bg-blue-600 bg-opacity-60 shadow-md rounded-lg backdrop-filter backdrop-blur-md border-2 border-opacity-20 border-white p-2 flex flex-col gap-3" style={{ backfaceVisibility: 'hidden'}}>
@@ -34,7 +75,7 @@ const CreditCard = ({ objeto }) => {
                 </div>
                 <div className="card-expiration-date">
                   <div className="text-white">
-                    vto {objeto.expiration}
+                    vto {objeto.expiration_date}
                   </div>
                 </div>
               </section>
@@ -51,7 +92,11 @@ const CreditCard = ({ objeto }) => {
             </section>
           </section>
       </div>
-      </div>
+      <CardSaldo objeto={objeto}/>
+        <button className="edit absolute p-1 text-colorFuente1Submenu hover:text-colorFuente2Submenu hover:bg-hoverBotonSubmenu transition-all duration-300 ease-in-out flex justify-center items-center rounded-md top-0 right-0 w-[35px] h-[35px] sm:w-[40px] sm:h-[40px] text-2xl" onClick={()=> setIsConfirmed(true)}>
+          <ion-icon name="trash"></ion-icon>         
+        </button>
+    </div>
   )
 }
 
--- client/src/components/CreditCardList.jsx
+++ client/src/components/CreditCardList.jsx
@@ -19,14 +19,14 @@ const CreditCardList = () => {
     <div className="flex w-5/6 sm:w-4/5 flex-col items-center justify-start sm:flex-row sm:items-start sm:justify-start bg-fondo h-auto p-0 sm:p-6 gap-4 sm:gap-6">
       {/* informacion usuario */}
       <section className="flex sm:flex-row justify-center items-center w-full h-auto gap-2 flex-wrap box-border pt-3">
-        <h2 className="w-full h-auto text-2xl font-titulo font-[700] text-[#0B0B0B] text-center">Informacion Personal</h2>
+        <h2 className="w-full h-auto text-2xl font-titulo font-[700] text-[#0B0B0B] text-center">Información Personal</h2>
 
-        <div className="w-full h-auto flex flex-col gap-3 border-solid border-[3px] border-colorFuente1Submenu rounded-sm p-1 box-border">
+        <div className="w-full h-auto flex flex-col sm:flex-row sm:justify-center items-center gap-3 sm:gap-5 p-1 box-border sm:flex-wrap">
 
           {
             cards.length > 0 ? cards.map((card) => {
               return (
-                <CreditCard key={card.id} objeto={card} />
+                <CreditCard key={card._id } objeto={card} />
               )
             }) : <NotItem />
           }
